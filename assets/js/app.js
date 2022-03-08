a5a42ff84702a2e88fd1c3c131bfc2f22b522676
Colin Shaw <colin.shaw400@outlook.com>
2022-03-08 16:47:44 +0000

    Add Start easy game function

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,44 @@
 // Wait for the DOM to finish loading before running the game
 // Get the button elements and add event listeners to them
 
+    // document,addEventListener("DOMContentLoaded", function(){
+    //     let buttons = document.getElementsByTagName('button');
+
+    //     for (let button of buttons) {
+    //         button.addEventListener("click", function() {
+    //             if (this.getAttribute("data-type") === "submit") {
+    //                 checkAnswer();
+    //             } else {
+    //                 let game = this.getAttribute("data-type");
+                   
+    //             }
+            
+
+    //     computerChoice = Math.random();
+    //     if (computerChoice < 0.2) {
+    //         computerChoice = "rock";
+    //     } else if (computerChoice <= 0.4) {
+    //         computerChoice = "paper";
+    //     } else if (computerChoice <= 0.6) {
+    //         computerChoice = "scissors";
+    //     } else if (computerChoice <= 0.8) {
+    //         computerChoice = "lizard";
+    //     } else {
+    //         computerChoice = "spock";
+    //     }
+    // });
 // document.addEventListener("DOMContentLoaded", function() {
 //     let buttons = document.getElementsByTagName("button");
 
-//     for (let button of buttons) {
-//         button.addEventListener("click", function() {
-//             if (this.getAttribute("data-type") === "submit") {
-//                 checkAnswer();
-//             } else {
-//                 let gameType = this.getAttribute("data-type");
-//                 runGame(gameType);
-//             }
-//         });
+    // for (let button of buttons) {
+    //     button.addEventListener("click", function() {
+    //         if (this.getAttribute("data-type") === "submit") {
+    //             checkAnswer();
+    //         } else {
+    //             let computerChoice = this.getAttribute("data-type");
+    //             compareSelect(computerChoice);
+    //         }
+    //     });
 //     }
 
 // });
@@ -20,6 +46,19 @@
 //Declare variables for player and computer secltions
 let playerSelection;
 let computerSelection;
+let pScore = 0; 
+let cScore = 0;
+
+
+/***
+ * Function to start the game 
+ */
+function startEasyGame(){
+    let easyBtn = document.getElementById('play-easy');
+
+    easyBtn.addEventListener("click");
+}
+
 
 /**
  * The main function that will use an if statement to compare each selction 
@@ -27,7 +66,7 @@ let computerSelection;
  */
 function compareSelect(computerChoice){
     if(playerSelection === computerChoice){
-        return "Computer has chosen " + computerSelection + "this round is a tie!";
+        return "Computer has chosen " + computerSelection + " this round is a tie!";
     }else if(playerSelection === "rock"){
         if(computerChoice === "lizard"){
             return "Rock crushes Lizard! Player Wins";
@@ -79,4 +118,5 @@ function compareSelect(computerChoice){
             return "Lizard poisons Spock! Computer Wins";
         }
     }
-}
+};
+compareSelect(playerSelection, computerSelection);
