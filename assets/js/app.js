8297fa6810b31f95f196e6d4563a77c446472b57
Colin Shaw <colin.shaw400@outlook.com>
2022-03-08 14:42:33 +0000

    Add function for all out comes of the player choices

--- assets/js/app.js
+++ assets/js/app.js
@@ -38,5 +38,45 @@ function compareSelect(computerChoice){
         }else{
             return "Paper covers Rock! Computer Wins";
         }
+    }else if(playerSelection =="paper"){
+        if(computerChoice === "scissors"){
+            return "Scissors cuts Paper! Computer Wins";
+        }else if(computerChoice === "lizard") {
+            return "Lizard eats Paper! Computer Wins";
+        }else if(computerChoice === "spock"){
+            return "Paper disproves Spock! Player Wins";
+        }else{
+            return "Paper covers Rock! Player Wins"
+        }
+    }else if(playerSelection === "scissors") {
+        if(computerChoice === "rock"){
+            return "Rock crushes Scissors! Computer Wins";
+        }else if(computerChoice === "paper"){
+            return "Scissors cuts Paper! Player Wins";
+        }else if(computerChoice === "lizard"){
+            return "Scissors decapitates Lizard! Player Wins";
+        }else{
+            return "Spock smashes Scissors";
+        }
+    }else if(playerSelection === "lizard"){
+        if(computerChoice === "rock"){
+            return "Rock crushes Lizard! Computer Wins";
+        }else if(computerChoice === "paper"){
+            return "Lizard eats Paper! Player Wins";
+        }else if(computerChoice === "scissors"){
+            return "Scissors decapitates Lizard! Computer Wins";
+        }else {
+            return "Lizard poisons Spock";
+        }
+    }else if(playerSelection === "spock"){
+        if(computerChoice === "rock"){
+            return "Spock vaporizes Rock! Player Wins";
+        }else if (computerChoice === "paper"){
+            return "Paper disproves Spock! Computer Wins";
+        }else if(computerChoice === "scissors"){
+            return "Spock smashes Scissors! Player Wins";
+        }else{
+            return "Lizard poisons Spock! Computer Wins";
+        }
     }
 }
