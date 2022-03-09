3e753528291b8306431df1e96a86a85ad2d3b78f
Colin Shaw <colin.shaw400@outlook.com>
2022-03-09 18:30:17 +0000

    Add Check winner function with if statment for all possible outcomes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,3 @@
-
 //Declaring variables 
 let playerSelection = document.getElementById("player-choice");
 let computerChoice = document.getElementById("computer-choice");
@@ -16,7 +15,7 @@ let selectionChoice = document.getElementsByClassName("choice-button");
 //     playerSelection.innerHTML = choice;
 // }))
 
-  for ( let i = 0 ; i < selectionChoice.length ; i++){
+for (let i = 0; i < selectionChoice.length; i++) {
     selectionChoice[i].addEventListener('click', runEasyGame);
 }
 
@@ -28,15 +27,72 @@ function runEasyGame(e) {
     playerSelection = e.target.innerHTML = choice;
 
     computerChoice = Math.random();
-    if (computerChoice < .24){
+    if (computerChoice < .24) {
         computerChoice = 'paper';
-    } else if (computerChoice <= .47){
+    } else if (computerChoice <= .47) {
         computerChoice = 'Paper';
-    } else if (computerChoice <= .60){
+    } else if (computerChoice <= .60) {
         computerChoice = 'Scissors';
-    }  else if (computerChoice <= .77){
+    } else if (computerChoice <= .77) {
         computerChoice = 'lizard';
-    }else {
+    } else {
         computerChoice = 'spock';
+    }
+}
+
+
+function checkWinner(playerSelection, computerChoice) {
+    if (playerSelection === computerChoice) {
+        return "Computer has chosen " + computerSelection + "this round is a tie!";
+    } else if (playerSelection === "rock") {
+        if (computerChoice === "lizard") {
+            return "Rock crushes Lizard! Player Wins";
+        } else if (computerChoice === "scissors") {
+            return "Rock crushes Scissors!! Player Wins";
+        } else if (computerChoice === "spock") {
+            return "Spock vaporizes Rock!! Computer Wins";
+        } else {
+            return "Paper covers Rock! Computer Wins";
+        }
+    } else if (playerSelection == "paper") {
+        if (computerChoice === "scissors") {
+            return "Scissors cuts Paper! Computer Wins";
+        } else if (computerChoice === "lizard") {
+            return "Lizard eats Paper! Computer Wins";
+        } else if (computerChoice === "spock") {
+            return "Paper disproves Spock! Player Wins";
+        } else {
+            return "Paper covers Rock! Player Wins"
+        }
+    } else if (playerSelection === "scissors") {
+        if (computerChoice === "rock") {
+            return "Rock crushes Scissors! Computer Wins";
+        } else if (computerChoice === "paper") {
+            return "Scissors cuts Paper! Player Wins";
+        } else if (computerChoice === "lizard") {
+            return "Scissors decapitates Lizard! Player Wins";
+        } else {
+            return "Spock smashes Scissors";
+        }
+    } else if (playerSelection === "lizard") {
+        if (computerChoice === "rock") {
+            return "Rock crushes Lizard! Computer Wins";
+        } else if (computerChoice === "paper") {
+            return "Lizard eats Paper! Player Wins";
+        } else if (computerChoice === "scissors") {
+            return "Scissors decapitates Lizard! Computer Wins";
+        } else {
+            return "Lizard poisons Spock";
+        }
+    } else if (playerSelection === "spock") {
+        if (computerChoice === "rock") {
+            return "Spock vaporizes Rock! Player Wins";
+        } else if (computerChoice === "paper") {
+            return "Paper disproves Spock! Computer Wins";
+        } else if (computerChoice === "scissors") {
+            return "Spock smashes Scissors! Player Wins";
+        } else {
+            return "Lizard poisons Spock! Computer Wins";
+        }
     }
 }
